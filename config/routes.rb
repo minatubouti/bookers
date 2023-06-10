adefca3d74e53ecba730218a51c1ad32609f9da3
minatubouti <pingneimeinai@gmail.com>
2023-06-10 14:37:10 +0000

    third commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  # get 'books/new'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/edit'
+   get '/top' => 'homes#top'
+   root to: 'homes#top'
+   resources :books, only: [:index,:new,:show,:edit]
+   post 'books' => 'books#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
