9fe434eb3bf38035d2031b56541323ee96c2bac2
minatubouti <pingneimeinai@gmail.com>
2023-06-11 12:34:01 +0000

    fourh commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   # get 'books/edit'
    get '/top' => 'homes#top'
    root to: 'homes#top'
-   resources :books, only: [:index,:new,:show,:edit]
+   resources :books, only: [:index,:new,:show,:edit,:create]
    post 'books' => 'books#create'
+   get 'books' => 'books#index'
+   get 'books/:id' => 'books#show'
+   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
