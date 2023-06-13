fde99b546dfe0483cfe50854125f14da10389cae
minatubouti <pingneimeinai@gmail.com>
2023-06-13 04:44:36 +0000

    fifth commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,14 +3,18 @@ class BooksController < ApplicationController
     @book = Book.new
   end
   
-  def create
+   def create
     @book = Book.new(book_params)
+    
    if @book.save
-   redirect_to books_path, notice: 'Book was successfully created.'
+   flash[:notice] = "Book was successfully created."
+   redirect_to book_path(@book.id)
    else
-     render :new
+   @books = Book.all
+   render :index
    end
   end
+ 
 
   def index
     @books = Book.all
@@ -25,10 +29,25 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
   
+    
+  def update
+  @book = Book.find(params[:id])
+
+  if @book.update(book_params)
+    flash[:notice] = 'Book was successfully updated.'
+    redirect_to book_path(@book)
+  else
+    render :edit
+  end
+end
+
+  
   def destroy
-    book = Book.find(params[:id])
+    book = Book.find(params[:id,])
     book.destroy
-    redirect_to '/books'
+    #フラッシュメッセージ
+    flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
   end
   
 private
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
    get 'books' => 'books#index'
    get 'books/:id' => 'books#show'
    delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-   
+   patch 'books/:id' => 'books#update', as: 'update_book'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
